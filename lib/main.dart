1295490363d4881974031bcb50ea601cdd5b865d
dizelvinable <sadr3479@gmail.com>
2021-06-11 15:37:43 +0300

    my ready version

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,63 @@ void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   final player = AudioCache();
+  final List<XylophoneButtonsData> xylophoneButtonsData = [
+    XylophoneButtonsData(Colors.red, 1),
+    XylophoneButtonsData(Colors.orange, 2),
+    XylophoneButtonsData(Colors.yellow, 3),
+    XylophoneButtonsData(Colors.green, 4),
+    XylophoneButtonsData(Colors.blue[200], 5),
+    XylophoneButtonsData(Colors.blue[900], 6),
+    XylophoneButtonsData(Colors.purple, 7),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                player.play('note1.wav');
-              },
-              child: Text('Click me!'),
-            ),
+          child: Column(
+            children: xylophoneButtonsData
+                .map(
+                  (e) => XylophoneButton(
+                      color: e.color, noteNumber: e.noteNumber, player: player),
+                )
+                .toList(),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class XylophoneButtonsData {
+  final Color color;
+  final int noteNumber;
+
+  XylophoneButtonsData(this.color, this.noteNumber);
+}
+
+class XylophoneButton extends StatelessWidget {
+  final Color color;
+  final int noteNumber;
+  final AudioCache player;
+
+  const XylophoneButton({Key key, this.color, this.noteNumber, this.player})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+        child: Container(
+          color: color,
+        ),
+        onPressed: () {
+          player.play('note$noteNumber.wav');
+        },
       ),
     );
   }
